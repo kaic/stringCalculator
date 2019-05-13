a8f9858f6ef25ac921273f18d7eed64c5f045f35
Kaic Bento <kaicbento@outlook.com>
2019-05-12 22:01:25 -0300

    supporting new lines instead only commas

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,33 @@
 const add = (string) => {
 	const reducerSum = (accumulator, currentValue) => accumulator + Number(currentValue);
-	const numbersToSum = string.split(',');
+
+	let numbersToSum = string.split(',');
+	let splitIndex;
+
+	let newNumbersToSum = numbersToSum.map((number, index, array) => { 
+
+		if(number.search('\n') == 1) {
+
+			let splitedNumbers = number.split('\n');
+			splitIndex = index;
+			return splitedNumbers;
+
+		}
+
+		return number;
+	});
+
+	if(splitIndex || splitIndex === 0) {
+		numbersToSum.splice(splitIndex, splitIndex + 1);
+
+		newNumbersToSum.map((number) => { 
+			if (Array.isArray(number)) {
+				number.map((n) => numbersToSum.unshift(n));
+			}
+		});
+	}
+
+	console.log(numbersToSum)
 
 	const result = numbersToSum.reduce(reducerSum, 0);
 
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -12,4 +12,14 @@ describe('String Calculator', () => {
     const result = stringCalculator.add("");
       assert.equal(result, 0);
     });
+
+    it('should return 10 when the value is "1,2,3,4"', () => {
+    const result = stringCalculator.add("1,2,3,4");
+      assert.equal(result, 10);
+    });
+
+    it('should return 6 when the value is "1\n2,3"', () => {
+    const result = stringCalculator.add("1\n2,3"); 
+      assert.equal(result, 6);
+    });
  });
